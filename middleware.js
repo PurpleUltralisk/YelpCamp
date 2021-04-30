31d103259c7e29cc6226d924fd0a1df9f3b075c8
PurpleUltralisk <uw.david.jiang@gmail.com>
2021-04-30 18:03:50 -0400

    refactoring

--- middleware.js
+++ middleware.js
@@ -46,7 +46,7 @@ module.exports.validateReview = (req, res, next)=>{
 module.exports.isReviewAuthor = async (req, res, next) => {
     const {id, reviewId} = req.params;
     const review = await Review.findById(reviewId);
-    if(!campground.author.equals(req.user._id)){
+    if(!review.author.equals(req.user._id)){
         req.flash('error', 'Do not have permission to do that');
         return res.redirect(`/campgrounds/${id}`);
     }
